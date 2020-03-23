7a2aa4ba137506b8900b36bb762be5195ac1fba3
Brad <brad@viziworx.com>
2020-03-23 13:48:24 -0400

    adds move_to_front

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -95,13 +95,19 @@ class DoublyLinkedList:
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
