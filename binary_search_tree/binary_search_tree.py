35df0a055946cb35831458920287205200d33f95
Brad <brad@viziworx.com>
2020-03-25 16:42:01 -0400

    adds an insert method that adds the input value to the binary search tree using recursion

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,7 +12,23 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+      # check if new value is less than current node
+      if value < self.value:
+        # if there is no self.left value
+        if not self.left:
+          # set the new left child to be new value
+          self.left = BinarySearchTree(value)
+        else:
+          self.left.insert(value)
+
+      # the new value is greater than the current node
+      # go right
+      else:
+        if not self.right:
+          self.right = BinarySearchTree(value)
+        else:
+          self.right.insert(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
